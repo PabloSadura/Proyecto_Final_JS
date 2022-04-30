974c4d31dd419bf3f3a0e936dab59e18f6d58b97
PabloSadura <pablosadura@gmail.com>
2022-04-30 14:22:27 -0300

    Agregado de msj en el alerta de contratar

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ let nombre = prompt("Ingresa tu nombre");
 alert("Bienvenido " + nombre);
 
 function alertaBasico() {
-  alert("Agregaste el paquete Básico al carrito");
+  alert("Gracias " + nombre + "\nAgregaste el paquete Básico al carrito");
 }
 function alertaIntermedio() {
-  alert("Agregaste el paquete Intermedio al carrito");
+  alert("Gracias " + nombre + "\nAgregaste el paquete Intermedio al carrito");
 }
 function alertaPremium() {
-  alert("Agregaste el paquete Premium al carrito");
+  alert("Gracias " + nombre + "\nAgregaste el paquete Premium al carrito");
 }
