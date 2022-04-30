153f8e8fbe3c48d1755e344f487ea4ee84360af3
PabloSadura <pablosadura@gmail.com>
2022-04-30 14:20:41 -0300

    agregado de alert y onclick

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-function alertaBasico(){
-    alert("Agregaste el paquete Básico al carrito");
+let nombre = prompt("Ingresa tu nombre");
+alert("Bienvenido " + nombre);
+
+function alertaBasico() {
+  alert("Agregaste el paquete Básico al carrito");
 }
-function alertaIntermedio(){
-    alert("Agregaste el paquete Intermedio al carrito");
+function alertaIntermedio() {
+  alert("Agregaste el paquete Intermedio al carrito");
 }
-function alertaPremium(){
-    alert("Agregaste el paquete Premium al carrito");
+function alertaPremium() {
+  alert("Agregaste el paquete Premium al carrito");
 }
