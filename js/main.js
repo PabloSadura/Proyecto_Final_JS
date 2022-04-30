9f20207099c833b992920d0539831dd264cfbbf7
PabloSadura <pablosadura@gmail.com>
2022-04-30 13:38:00 -0300

    agrague carrito y btn

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function alertaBasico(){
+    alert("Agregaste el paquete Básico al carrito");
+}
+function alertaIntermedio(){
+    alert("Agregaste el paquete Intermedio al carrito");
+}
+function alertaPremium(){
+    alert("Agregaste el paquete Premium al carrito");
+}
